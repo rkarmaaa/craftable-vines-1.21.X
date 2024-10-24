b7f3d7120952c288c5a5ee1c633d22a2993b6882
Marco <luccimarco176@gmail.com>
2024-10-25 01:33:05 +0200

    Initial commit

---
+++ src/client/java/craftable/vines/CraftableVinesClient.java
@@ -0,0 +1,10 @@
+package craftable.vines;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CraftableVinesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/craftable/vines/CraftableVines.java
@@ -0,0 +1,15 @@
+package craftable.vines;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CraftableVines implements ModInitializer {
+	public static final String MOD_ID = "craftable-vines";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
